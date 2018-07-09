9e0073e4115f7eca745b5fb58be76ce45a48e51d
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-09 10:28:32 -0600

    Initial Commit

---
+++ iOS-Music-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Music-App
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/9/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
